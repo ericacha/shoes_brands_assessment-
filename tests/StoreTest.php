7eabbbd2c07a7f78b1a4961d9cd34a9d30fc9f96
Erica <chae_osu@yahoo.com>
2015-03-27 10:25:42 -0700

    test getName() pass.

---
+++ tests/StoreTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Store.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=shoe_test');
+
+    class StoreTest extends PHPUnit_Framework_TestCase
+
+    {
+      // protected function tearDown()
+      //   {
+      //       Store::deleteAll();
+      //   }
+
+
+      function test_getName()
+      {
+        //Arrange
+        $name = "Nike";
+        $id = null;
+        $test_store = new Store($name, $id);
+
+        //Act
+        $result= $test_store->getName();
+
+        //Assert
+        $this->assertEquals("Nike", $result);
+      }
+      //   function test_getId()
+      // {
+      //   //Arrange
+      //   $name = "Prada";
+      //   $id = 1;
+      //   $test_store = new Store($name, $id);
+      //
+      //   //Act
+      //   $result = $test_store->getId();
+      //
+      //   //Arrange
+      //   $this->assertEquals(1, $result);
+      // }
+      //
+      // function test_setId()
+      // {
+      //   //Arrange
+      //   $name = "Prada";
+      //   $id = 2;
+      //   $test_store = new Store($name, $id);
+      //   $new_id = 4;
+      //
+      //   //Act
+      //   $test_store->setId($new_id);
+      //   $result = $test_store->getId();
+      //
+      //   //Arrange
+      //   $this->assertEquals(4, $result);
+      //
+      // }
+
+
+    }
+?>
