d4faa8a9a78f3e917f41d1dd354e4415666ee508
Erica <chae_osu@yahoo.com>
2015-04-01 18:22:32 -0700

    Twig for store.html to click on individual link success.

--- app/app.php
+++ app/app.php
@@ -15,45 +15,57 @@
         Request::enableHttpMethodParameterOverride();
 
     //GET
+
     //this is the main page for user
     $app->get('/', function() use ($app) {
-      return $app['twig']->render("index.html.twig");
+      return $app['twig']->render('index.html.twig');
     });
-    //this is what the store page looks like when clicking to add/view shoe stores
+    //#2 this is what the store page looks like when clicking to add/view shoe stores
     $app->get("stores", function () use ($app) {
-      return $app['twig']->render("stores.html.twig", array('stores' => Store::getAll()));
+      return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
 
     });
 
-    //this is what the brand page looks like when clicking to add/view brand shoes
+    //#3 this is what the brand page looks like when clicking to add/view brand shoes
     $app->get("brands", function() use ($app) {
-      return $app['twig']->render("brands.html.twig", array('brands' => Brand::getAll()));
+      return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
     });
 
-
-    $app->get("/stores/{id}", function() use ($app) {
-      $store = Store::find($id);
-      return $app['twig']->render("brand.html.twig", array('store' => $store, 'brands' => $store->getBrand(), 'all_brands' => Brand::getAll()));
-    });
-
+    //#4 this will continue  their list from the link to their individual page
     $app->get("/stores/{id}", function($id) use ($app) {
-
+      $store = Store::findId($id);
+      return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrand(),'all_brands' => Brand::getAll()));
     });
+
+
+
 
     //POST
-    //this is where the user gets to input what store name they want to add onto page
+
+    //#2 this is where the user gets to input what store name they want to add onto page
     $app->post("/stores", function() use ($app) {
-      $name = $_POST['name'];
-      $store = new Store($name);
+      $store = new Store($_POST['name']);
       $store->save();
       return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
-    $app->post("/brands", function() use ($app) {
-      $type = $_POST['type'];
-      $brand = new Brand($type);
-      $brand->save();
-      return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    //#3 this is where the user gets to input what brand name they want to add onto page
+    // $app->post("/brands", function() use ($app) {
+    //   $brand = new Brand($_POST['type']);
+    //   $brand->save();
+    //   return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    // });
+
+    //this will delete the list of stores
+    $app->post("/delete_stores", function() use ($app) {
+      Store::deleteAll();
+      return $app['twig']->render('index.html.twig');
+    });
+
+    //this will delete the list of brands
+    $app->post("/delete_brands", function() use ($app) {
+      Brand::deleteAll();
+      return $app['twig']->render('index.html.twig');
     });
 
 
